0974b0388d0bf47d9933f9e41fd521d6f4ec597a
Idontlikeni <tima.xxxyyy@mail.ru>
2025-03-03 11:53:01 +0300

    misc on laptop

--- piton/pr_1.py
+++ piton/pr_1.py
@@ -1,13 +1,9 @@
 import math as m
 
 
-def main(b, n, a):
-    smbk = 0
-    for k in range(1, n + 1):
-        for j in range(1, b + 1):
-            smbk += 75 * (22 * k ** 2 - 51 - 69 * j) + m.ceil(k) ** 4
-    smab = 0
-    for j in range(1, a + 1):
-        for i in range(1, b + 1):
-            smab += (abs(j/19) ** 4 + 0.01 + 97*i**2)
-    return smbk - smab
+def main(n):
+    if n == 0:
+        return -0.48
+    elif n >= 1:
+        m = main(n-1)
+        return 1 - 2 * m ** 2 - (2 * m ** 3 - m) ** 3
