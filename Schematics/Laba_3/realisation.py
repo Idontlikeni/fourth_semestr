63bbbd1aa137aa2c2a702e7317fdba7db2e43ad7
Timur Tuktarov <tima.xxxyyy@mail.ru>
2025-03-19 07:25:51 +0300

    laba_3 started (py + block_scheme)

--- Schematics/Laba_3/realisation.py
+++ Schematics/Laba_3/realisation.py
@@ -5,4 +5,17 @@ def print_array(arr):
 
 
 def subsort(arr, n):
+    nums = [0 for _ in range(10)]
+    sorted_arr  = [0 for _ in range(n)]
+    counter = 0
+    for i in arr:
+        nums[i]+=1
+
+    for i in range(10):
+        for _ in range(nums[i]):
+            sorted_arr[counter] = i
+            counter += 1
     
+    return sorted_arr
+
+print(subsort([4, 7, 3, 2, 5, 6, 3, 1], 8))
