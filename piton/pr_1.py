14ba5916e78a89dbcae77382b94fe7e98ba20dbe
Idontlikeni <tima.xxxyyy@mail.ru>
2025-04-15 20:04:35 +0300

    Pr5 bazza

--- piton/pr_1.py
+++ piton/pr_1.py
@@ -1,34 +1,16 @@
-import math as m
 
+def main(s):
+    dct = dict()
 
-def main2(n):
-    b = bin(int(n, 16))[2:]
-    arr = [9, 10, 9, 4, 7, 3]
-    c = 0
-    y = []
-    for i in arr:
-        y.append(b[c:c+i])
-        c += i
-    print(y)
-    return int(y[1] + y[4] + y[2] + y[0] + y[3] + y[5], 2)
+    s = s.replace("\n", " ")
+    s = s.replace("<:", "").replace(":>", "").replace("do var ", "")
+    s = s.replace("array(", "").replace(")", "")
+    s = s.split("end")
+    s = [i.split(":=") for i in s]
 
-def main(n):
-    m = int(n, 16)
-    y = []
-    arr = [9, 10, 9, 4, 7, 3]
-    arr.reverse()
-    for i in arr:
-        temp = 2 ** i - 1
-        y.append(m & temp)
-        m = m >> i
-    indexes = [1, 4, 2, 0, 3, 5]
-    indexes.reverse()
-    res = 0
-    c = 0
-    for i in indexes:
-        y[i] = y[i] << c
-        res += y[i]
-        c += arr[i]
-    return res
+    for i in s:
+        if(len(i) == 2):
+            dct[i[0].strip()] = list(map(int, i[1].strip().split(',')))
+    return dct
 
-print(main('0x1280766daae'))
+#  print(main('<: do var eroror_601 := array(2816 , -8595 , -5388 , 2978 )end do var razati_883 := array( -9709 , -2010 , -7553 , 2)end do var erdi:= array( 6400 , -5978 ,173 )end:>'))
